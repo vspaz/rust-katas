8391dba02c16b3d1aff6e6acfae0b8f27e9a996c
vspaz <vladimir.edelman@gmail.com>
2022-12-21 19:59:22 +0100

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod math;
 use math::digitcounter::get_digit_count;
+use math::reversedigit::reverse_digit;
 
 fn main() {
    assert_eq!(5, get_digit_count(10303));
+   assert_eq!(54321, reverse_digit(12345));
 }
