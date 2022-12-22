47a544106a6eff5966d9d45b0aaf6a89cc4552c0
vspaz <vladimir.edelman@gmail.com>
2022-12-22 14:02:07 +0100

    style fixes

--- src/math/reversedigit.rs
+++ src/math/reversedigit.rs
@@ -1,13 +1,12 @@
 pub fn reverse_digit(num: i64) -> i64 {
     let mut reversed_num = 0;
     let mut original_num = num;
-    while  original_num > 0 {
+    while original_num > 0 {
         reversed_num = reversed_num * 10 + original_num % 10;
         original_num /= 10;
     }
     return reversed_num;
 }
-
 
 #[cfg(test)]
 mod tests {
