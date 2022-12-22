6c064ffccd2db40c63f0d7f969684d5e44309a84
vspaz <vladimir.edelman@gmail.com>
2022-12-22 19:02:15 +0100

    various fixes

--- src/stringops/lettercounter.rs
+++ src/stringops/lettercounter.rs
@@ -1,23 +1,23 @@
 use std::collections::HashMap;
 
-pub fn count_letters(text: String) -> HashMap<&str, i32> {
-    let mut letter_to_count = HashMap::with_capacity(text.len());
+pub fn count_letters(text: &str) -> HashMap<char, i32> {
+    let mut letter_to_count: HashMap<char, i32> = HashMap::with_capacity(text.len());
     for letter in text.chars() {
-        if !letter_to_count.contains_key(letter) {
-            letter_to_count.insert(letter, 0)
+        if !letter_to_count.contains_key(&letter) {
+            letter_to_count.insert(letter, 0);
         }
-        letter_to_count.insert(letter, letter_to_count.get(letter) + 1);
+        letter_to_count.insert(letter, letter_to_count[&letter] + 1);
     }
-    return letter_to_count
+    return letter_to_count;
 }
 
-pub fn count_letters_2(text: String) -> HashMap<&str, i32> {
-    let mut letter_to_count = HashMap::with_capacity(text.len());
+pub fn count_letters_2(text: &str) -> HashMap<char, i32> {
+    let mut letter_to_count: HashMap<char, i32> = HashMap::with_capacity(text.len());
     for letter in text.chars() {
-        letter_to_count.
-            entry(letter).
-            and_modify(|count| *count += 1).
-            or_insert(1);
+        letter_to_count
+            .entry(letter)
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
     }
     return letter_to_count;
 }
