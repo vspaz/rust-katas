0adb61d242de8dec3f4c444c789dd14aaeaa399e
vspaz <vladimir.edelman@gmail.com>
2022-12-23 18:10:13 +0100

    simplify paths

--- src/bin/math/main.rs
+++ src/bin/math/main.rs
@@ -1,10 +1,7 @@
 mod digitcounter;
 mod reversedigit;
 
-use digitcounter::get_digit_count;
-use reversedigit::reverse_digit;
-
 fn main() {
-    assert_eq!(5, get_digit_count(10303));
-    assert_eq!(54321, reverse_digit(12345));
+    assert_eq!(5, digitcounter::get_digit_count(10303));
+    assert_eq!(54321, reversedigit::reverse_digit(12345));
 }
--- src/bin/stringops/main.rs
+++ src/bin/stringops/main.rs
@@ -1,8 +1,6 @@
 mod lettercounter;
-use lettercounter::count_letters;
-use lettercounter::count_letters_2;
 
 fn main() {
-    assert_eq!(2, count_letters("foo")[&'o']);
-    assert_eq!(2, count_letters_2("foo")[&'o']);
+    assert_eq!(2, lettercounter::count_letters("foo")[&'o']);
+    assert_eq!(2, lettercounter::count_letters_2("foo")[&'o']);
 }
