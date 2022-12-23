55b1f27cca41c2f643b0a3888132d17ca9d030c5
vspaz <vladimir.edelman@gmail.com>
2022-12-23 13:54:43 +0100

    remove extra variable

--- src/math/digitcounter.rs
+++ src/math/digitcounter.rs
@@ -1,8 +1,7 @@
-pub fn get_digit_count(num: u32) -> u32 {
+pub fn get_digit_count(mut num: u32) -> u32 {
     let mut count: u32 = 0;
-    let mut remainder = num;
-    while remainder > 0 {
-        remainder /= 10;
+    while num > 0 {
+        num /= 10;
         count += 1;
     }
     return count;
