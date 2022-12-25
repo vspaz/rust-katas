e540e9aa92f3d4e910238cd629ad8d326fce88cd
vspaz <vladimir.edelman@gmail.com>
2022-12-25 10:00:14 +0100

    add test

--- src/bin/recursion/factorial.rs
+++ src/bin/recursion/factorial.rs
@@ -4,3 +4,13 @@ pub fn get_factorial(num: i64) -> i64 {
     }
     return num * get_factorial(num - 1);
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::factorial::get_factorial;
+
+    #[test]
+    fn test_get_factorial_ok() {
+        assert_eq!(120, get_factorial(5));
+    }
+}
