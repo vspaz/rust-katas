d30024cb231dd9f15800e3e3dd5a7f25c8484d81
vspaz <vladimir.edelman@gmail.com>
2022-12-25 14:51:50 +0100

    add test

--- src/bin/math/fibonacci.rs
+++ src/bin/math/fibonacci.rs
@@ -1,7 +1,17 @@
 pub fn fib(num: i64) -> i64 {
-    let (mut a,mut b): (i64, i64) = (1, 1);
-    for i in 1..num {
+    let (mut a, mut b): (i64, i64) = (1, 1);
+    for _ in 1..num {
         (a, b) = (b, a + b);
     }
     a
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::fibonacci::fib;
+
+    #[test]
+    fn test_fib_ok() {
+        assert_eq!(55, fib(10));
+    }
 }
