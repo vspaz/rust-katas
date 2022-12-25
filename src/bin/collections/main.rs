378f87f770ddb148b370b0d72c71981ff403731e
vspaz <vladimir.edelman@gmail.com>
2022-12-25 17:24:21 +0100

    make clippy happy

--- src/bin/collections/main.rs
+++ src/bin/collections/main.rs
@@ -4,4 +4,6 @@ fn main() {
     let mut nums: Vec<i64> = vec![1, 2, 3, -10, 40, 100, -80, 50, 140];
     nums.insert(0, 10);
     assert_eq!(arrays::get_max(nums), 140);
+
+    assert_eq!(10, arrays::get_total(vec![2, 2, 2, 2, 2]));
 }
--- src/bin/recursion/factorial.rs
+++ src/bin/recursion/factorial.rs
@@ -2,7 +2,7 @@ pub fn get_factorial(num: i64) -> i64 {
     if num == 1 {
         return 1;
     }
-    return num * get_factorial(num - 1);
+    num * get_factorial(num - 1)
 }
 
 #[cfg(test)]
