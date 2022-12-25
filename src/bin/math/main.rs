20ffcf983411a692d34724fae6252fdc0702c2bd
vspaz <vladimir.edelman@gmail.com>
2022-12-25 14:52:10 +0100

    update main

--- src/bin/math/main.rs
+++ src/bin/math/main.rs
@@ -1,9 +1,11 @@
 mod digitcounter;
 mod factorial;
+mod fibonacci;
 mod reversedigit;
 
 fn main() {
     assert_eq!(5, digitcounter::get_digit_count(10303));
     assert_eq!(54321, reversedigit::reverse_digit(12345));
     assert_eq!(120, factorial::get_factorial(5));
+    assert_eq!(55, fibonacci::fib(10));
 }
