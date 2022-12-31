319d78de9c2a8fc1b008f1868be5ee1d00866e9c
vspaz <vladimir.edelman@gmail.com>
2022-12-31 13:37:18 +0100

    fix type to usize

--- src/bin/search/binary_search.rs
+++ src/bin/search/binary_search.rs
@@ -1,9 +1,9 @@
-pub fn bin_search(nums: Vec<i64>, num: i64) -> Option<i64> {
+pub fn bin_search(nums: Vec<i64>, num: i64) -> Option<usize> {
     let mut lower_bound = 0;
     let mut upper_bound = nums.len() - 1;
 
     while lower_bound <= upper_bound {
-        mid = (lower_bound + upper_bound) / 2;
+        let mid = (lower_bound + upper_bound) / 2;
         let guess = nums[mid];
         if guess == num {
             return Some(mid);
--- src/bin/search/main.rs
+++ src/bin/search/main.rs
@@ -1,1 +1,6 @@
-fn main() {}
+mod binary_search;
+
+fn main() {
+    let nums: Vec<i64> = vec![-5, 0, 143, 800, 14800];
+    let num_index = binary_search::bin_search(nums, 143);
+}
