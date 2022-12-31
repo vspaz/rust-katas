3ff70fcdeb2f639547d618df9f7f25abf217eb36
vspaz <vladimir.edelman@gmail.com>
2022-12-31 13:42:42 +0100

    add an example

--- src/bin/search/main.rs
+++ src/bin/search/main.rs
@@ -2,5 +2,11 @@ mod binary_search;
 
 fn main() {
     let nums: Vec<i64> = vec![-5, 0, 143, 800, 14800];
-    let num_index = binary_search::bin_search(nums, 143);
+    let num_to_look_for = 143;
+    let num_index = binary_search::bin_search(nums, num_to_look_for);
+
+    match num_index {
+        Some(idx) => println!("num {} found at index {}", num_to_look_for, idx),
+        None => println!("num {} not found", num_to_look_for ),
+    }
 }
