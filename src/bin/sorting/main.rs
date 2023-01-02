9d98775fa7292d30a70de6f132ba611e9ddb2966
vspaz <vladimir.edelman@gmail.com>
2023-01-02 12:52:45 +0100

    add test; minor fixes

--- src/bin/sorting/main.rs
+++ src/bin/sorting/main.rs
@@ -1,1 +1,7 @@
+mod sorting;
 
+fn main() {
+    let mut nums = [5, 4, 3, 2, 1];
+    sorting::bubble_sort(&mut nums);
+    assert_eq!([1, 2, 3, 4, 5], nums);
+}
---
+++ src/bin/sorting/sorting.rs
@@ -0,0 +1,21 @@
+pub fn bubble_sort<T: Ord>(nums: &mut [T]) {
+    for i in 0..nums.len() {
+        for j in 0..nums.len() - (1 + i) {
+            if nums[j] > nums[j + 1] {
+            nums.swap(j, j+ 1);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::sorting;
+
+    #[test]
+    fn test_bubble_sort_ok() {
+        let mut nums = [5, 4, 3, 2, 1];
+        sorting::bubble_sort(&mut nums);
+        assert_eq!([1, 2, 3, 4, 5], nums);
+    }
+}
