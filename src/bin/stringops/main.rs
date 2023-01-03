be583614b44ef5357b2945fb5512ff0ccc6922d1
vspaz <vladimir.edelman@gmail.com>
2023-01-03 21:52:47 +0100

    update main

--- src/bin/stringops/main.rs
+++ src/bin/stringops/main.rs
@@ -1,7 +1,8 @@
 mod lettercounter;
-
+mod reverse;
 
 fn main() {
     assert_eq!(2, lettercounter::count_letters("foo")[&'o']);
     assert_eq!(2, lettercounter::count_letters_2("foo")[&'o']);
+    assert_eq!("12345", reverse::reverse_acii_string("54321"));
 }
