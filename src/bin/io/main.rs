6d721b820a6483a2a7b831f4b17d5e242d4c7ae1
vspaz <vladimir.edelman@gmail.com>
2023-03-22 21:46:47 +0100

    add console1.rs

--- src/bin/io/main.rs
+++ src/bin/io/main.rs
@@ -1,10 +1,1 @@
-use std::io;
 
-fn main() {
-    println!("pls, type in a word!");
-    let mut word = String::with_capacity(32);
-    io::stdin()
-        .read_line(&mut word)
-        .expect("failed to read the word");
-    println!("{}", word)
-}
