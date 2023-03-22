fd5cb360c0d4db369675b3833fba5b604afa39d1
vspaz <vladimir.edelman@gmail.com>
2023-03-22 22:22:39 +0100

    stylefixes

--- src/bin/io/console2.rs
+++ src/bin/io/console2.rs
@@ -1,5 +1,5 @@
-use std::io;
 use rand::Rng;
+use std::io;
 
 pub fn get_random_num() {
     let random_num = rand::thread_rng().gen_range(0..10);
@@ -9,6 +9,9 @@ pub fn get_random_num() {
     io::stdin()
         .read_line(&mut num_as_string)
         .expect("failed to read number from console");
-    num_as_string.pop();  // same as truncate truncate(num_as_string.len() - 1)
-    println!("your number is '{}', generated num is '{}'", num_as_string, random_num)
+    num_as_string.pop(); // same as truncate truncate(num_as_string.len() - 1)
+    println!(
+        "your number is '{}', generated num is '{}'",
+        num_as_string, random_num
+    )
 }
--- src/bin/io/main.rs
+++ src/bin/io/main.rs
@@ -2,6 +2,6 @@ mod console1;
 mod console2;
 
 fn main() {
-    //console1::get_text_from_console();
+    console1::get_text_from_console();
     console2::get_random_num();
 }
