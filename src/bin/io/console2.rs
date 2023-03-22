1aff0a7158830361173ef146bed47779812dea19
vspaz <vladimir.edelman@gmail.com>
2023-03-22 22:22:02 +0100

    remove trailing \n

--- src/bin/io/console2.rs
+++ src/bin/io/console2.rs
@@ -9,5 +9,6 @@ pub fn get_random_num() {
     io::stdin()
         .read_line(&mut num_as_string)
         .expect("failed to read number from console");
+    num_as_string.pop();  // same as truncate truncate(num_as_string.len() - 1)
     println!("your number is '{}', generated num is '{}'", num_as_string, random_num)
 }
