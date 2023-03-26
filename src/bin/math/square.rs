4f2d40f2b41b1a1f5bf123d8b99fc08bd48736f4
vspaz <vladimir.edelman@gmail.com>
2023-03-26 15:44:38 +0200

    minor style fixes

--- src/bin/math/square.rs
+++ src/bin/math/square.rs
@@ -6,7 +6,7 @@ pub(crate) struct Rectangle {
 
 impl Rectangle {
     pub fn new(width: u32, height: u32) -> Rectangle {
-        Rectangle {width, height}
+        Rectangle { width, height }
     }
     pub fn get_area(&self) -> u32 {
         self.width * self.height
