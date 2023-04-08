91d67b7e392a25ddbeb4ce6d225e05f97a00715c
vspaz <vladimir.edelman@gmail.com>
2023-04-08 14:26:08 +0200

    style fixes

--- src/bin/clis/cmd.rs
+++ src/bin/clis/cmd.rs
@@ -3,13 +3,13 @@ use std::env;
 #[derive(Debug)]
 pub struct CmdArgs {
     query: String,
-    file_path: String
+    file_path: String,
 }
 
 pub fn get_args() -> CmdArgs {
     let args: Vec<String> = env::args().collect();
-    return CmdArgs{
+    return CmdArgs {
         query: args[1].to_owned(),
         file_path: args[2].to_owned(),
-    }
+    };
 }
