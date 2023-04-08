7b5e1856c42435bb19c9474c3c10beb954637e75
vspaz <vladimir.edelman@gmail.com>
2023-04-08 14:40:33 +0200

    add minimal validation

--- src/bin/clis/cmd.rs
+++ src/bin/clis/cmd.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, process};
 
 #[derive(Debug)]
 pub struct CmdArgs {
@@ -8,6 +8,10 @@ pub struct CmdArgs {
 
 pub fn get_args() -> CmdArgs {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        println!("not enough arguments");
+        process::exit(-1)
+    }
     return CmdArgs {
         query: args[1].to_owned(),
         file_path: args[2].to_owned(),
