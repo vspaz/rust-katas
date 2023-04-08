d213767537d584528805cdbd626507172d328bd8
vspaz <vladimir.edelman@gmail.com>
2023-04-08 14:25:49 +0200

    add CmdArgs

--- src/bin/clis/cmd.rs
+++ src/bin/clis/cmd.rs
@@ -1,6 +1,15 @@
 use std::env;
 
+#[derive(Debug)]
+pub struct CmdArgs {
+    query: String,
+    file_path: String
+}
 
-pub fn get_args() -> Vec<String>{
-    return env::args().collect();
+pub fn get_args() -> CmdArgs {
+    let args: Vec<String> = env::args().collect();
+    return CmdArgs{
+        query: args[1].to_owned(),
+        file_path: args[2].to_owned(),
+    }
 }
